310e4d7c6ab74fdfd9b156c25c0a1d6e3acf814f
Whatisinternet <joshteeter@gmail.com>
2017-04-30 16:10:38 -0400

    Added new parser for csv in both rust and ruby. Basically taking the sample from the Rust CSV library

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -1,5 +1,18 @@
+require 'csv'
+
 class SampleController < ApplicationController
-  def foo
-    render json: {foo: "bar".thing?}
+  def rust
+    csv_path = Rails.root.join('vendor', 'bench.csv')
+    rusty_csv = RustyCSV.new.csv_to_json(csv_path.to_s)
+    render plain: rusty_csv
+  end
+
+  def ruby
+    csv_path = Rails.root.join('vendor', 'bench.csv')
+    output = []
+    CSV.foreach(csv_path.to_s, headers: true) do |thinger|
+      output << thinger.to_h
+    end
+    render json: output
   end
 end
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,46 @@
 #[macro_use]
 extern crate ruru;
+extern crate csv;
+extern crate rustc_serialize;
+use rustc_serialize::json;
 
-use ruru::{Boolean, Class, Object, RString};
+use ruru::{Class, Object, RString};
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct Record {
+    gameid: String,
+    qtr: u32,
+    min: Option<u32>,
+    sec: Option<u32>,
+    team_off: String,
+    team_def: String,
+    down: Option<u32>,
+    togo: Option<u32>,
+    ydline: Option<u32>,
+    description: String,
+    offscore: u32,
+    defscore: u32,
+    season: u32,
+}
 
 methods!(
    RString,
    itself,
 
-   fn string_is_blank() -> Boolean {
-       Boolean::new(itself.to_string().chars().all(|c| c.is_whitespace()))
+   fn csv_to_json(path: RString) -> RString {
+       let mut rdr = csv::Reader::from_file(path.unwrap_or(RString::new("")).to_string()).unwrap();
+       let mut records: Vec<Record> = vec!();
+       for record in rdr.decode() {
+           let rcrd: Record = record.unwrap();
+           records.push(rcrd);
+       }
+       RString::new(&json::encode(&records).unwrap())
    }
 );
 
 #[no_mangle]
 pub extern fn initialize_my_app() {
-    Class::from_existing("String").define(|itself| {
-        itself.def("thing?", string_is_blank);
+    Class::new("RustyCSV", None).define(|itself| {
+        itself.def("csv_to_json", csv_to_json);
     });
 }
